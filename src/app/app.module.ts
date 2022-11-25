b9b214a6a88021f8a9ea053fc40ac0586ecc73bc
delmashajan <delmashajan@gmail.com>
2022-11-26 00:53:56 +0530

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,36 @@ import { AppComponent } from './app.component';
 import { AddFriendComponent } from './add-friend/add-friend.component';
 import { SearchFriendComponent } from './search-friend/search-friend.component';
 import { ViewfriendComponent } from './viewfriend/viewfriend.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddFriendComponent
+  },
+  {
+    path:"search",
+    component:SearchFriendComponent
+  },
+  {
+    path:"view",
+    component:ViewfriendComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddFriendComponent,
     SearchFriendComponent,
-    ViewfriendComponent
+    ViewfriendComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
